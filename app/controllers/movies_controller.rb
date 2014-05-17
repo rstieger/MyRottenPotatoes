26770b7b72251f852772a6817ba3646070050e2b
Ron Stieger <ronstieger@gmail.com>
2014-05-17 00:44:52 -0300

    add Destroy method and link from details view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,4 +27,11 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "#{@movie.title} deleted."
+    redirect_to movies_path
+  end
 end
